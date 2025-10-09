e139f2fb7510c4641f9f24d409e09d24aa9ed637
zZBANK22Zz <ananda2546@gmail.com>
2025-10-09 14:36:14 +0700

    dev ecp

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = 8000
+const cors = require('cors')
+app.use(cors())
+
+const ecpRoutes = require('./app/routes/ecp.routes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/ecp', ecpRoutes);
 
 app.get('/', (req, res)=>{
     res.send(`App running on port${PORT}`);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${3000}`);
+    console.log(`Server running on port: ${PORT}`);
 })
